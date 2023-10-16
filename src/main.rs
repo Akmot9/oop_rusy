0e0d02f2e91a3a3b05f342d1e04a66f5467b5993
akmot9 <avicocyprien@yahoo.com>
2023-10-16 09:41:39 +0200

    init

---
+++ src/main.rs
@@ -0,0 +1,65 @@
+use pnet::datalink::Channel::Ethernet;
+use pnet::datalink;
+use pnet::packet::ethernet::EthernetPacket;
+use pnet::packet::Packet;
+use pnet::packet::FromPacket;
+use std::thread;
+
+fn main() {
+    let interfaces = datalink::interfaces();
+    let mut handles = vec![];
+
+    for interface in interfaces {
+        let handle = thread::spawn(move || {
+            capture_packets(interface);
+        });
+        handles.push(handle);
+    }
+    // Wait for all threads to complete
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    fn capture_packets(interface: datalink::NetworkInterface) {
+    let (_, mut rx) = match datalink::channel(&interface, 
+                                                                    Default::default()) {
+        Ok(Ethernet(tx, rx)) => (tx, rx),
+        Ok(_) => panic!("Unhandled channel type: {}",&interface),
+        Err(e) => panic!(
+            "An error occurred when creating the datalink channel: {}",
+            e
+        ),
+    };
+
+    println!("Start thread reading packet on interface: {}", &interface);
+        loop {
+            match rx.next() {
+                Ok(packet) => {
+                    if let Some(ethernet_packet) = EthernetPacket::new(packet) {
+                        println!("New packet on {}", interface.name);
+                        println!("{} => {}: {}",
+                            ethernet_packet.get_destination(),
+                            ethernet_packet.get_source(),
+                            ethernet_packet.get_ethertype());
+                        let packet = ethernet_packet.packet();
+                        let payload = ethernet_packet.payload();
+                        let from_packet = ethernet_packet.from_packet();
+                        //println!("---");
+                        println!("packet: {:?}", packet);
+                        // print the full packet as an array of u8
+                        println!("payload: {:?}", payload);
+                        // print the payload as an array of u8
+                        println!("from_packet: {:?}", from_packet);
+                        // print the hearder infos: mac address, ethertype, ...
+                        // and the payload as an array of u8
+                        println!("---");
+                        
+                    }
+                }
+                Err(e)=> {
+                    panic!("An error occurred while reading: {}", e);
+                }
+            }
+        }
+    }
+}
